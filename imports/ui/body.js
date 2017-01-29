044a1c265841d6bfdfc1f8f67223fc0c1a902463
R. Steve Young <r.steve.young@gmail.com>
2017-01-29 17:11:38 -0300

    Add basic create task functionality

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -9,3 +9,29 @@ Template.body.helpers({
 		return Tasks.find({});
 	},
 });
+
+Template.body.events({
+	'submit .new-task'(event) {
+		// Prevent default browser form submit
+		event.preventDefault();
+
+		// Get value from form element
+		const target = event.target;
+		const text = target.text.value;
+		console.log(event);
+
+		// Insert a task into the collection
+		Tasks.insert({
+			text,
+			createdAt: new Date(),
+		});
+
+		target.text.value = '';
+	}
+});
+
+
+
+
+
+
